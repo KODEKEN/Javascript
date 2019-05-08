e6cf83508f096f78385c9f4f175393f194d5c0f9
KODEKEN <37243210+KODEKEN@users.noreply.github.com>
2019-05-09 06:57:06 +0700

    Create safelink-full-page.js

---
+++ safelink-full-page.js
@@ -0,0 +1,6 @@
+var safelink = {
+  converter: 'https://slk21.blogspot.com/p/pengertian-asuransi.html,https://slk21.blogspot.com/p/how-to-see-whats-trending-on.html,https://slk21.blogspot.com/p/new-toyota-yaris.html',// Link Converter
+  link: 'fwsoft182.blogspot.com,linkedin.com,twitter.com,youtube.com,facebook.com,blogger.com,javascript:void(0);,javascript:;,bt-menu,###,exabytes.com,javascript:void(0)', // Masukan link yang tidak ingin di convert otomatis.
+  system: 2,
+  password: '' // Masukan password atau kosongkan saja
+}
